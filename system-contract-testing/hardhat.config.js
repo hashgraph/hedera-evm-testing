fbba2ee8f6dfdad4aae4f4a9b7d0bbd6b7285836
Glib Kozyryatskyy <glib.k@swirldslabs.com>
2025-08-18 15:52:40 +0300

    feat: reformat with prettier
    Signed-off-by: Glib Kozyryatskyy <glib.k@swirldslabs.com>

--- system-contract-testing/hardhat.config.js
+++ system-contract-testing/hardhat.config.js
@@ -1,70 +1,69 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 
-require('hardhat-abi-exporter');
-require('@openzeppelin/hardhat-upgrades');
-require('@nomicfoundation/hardhat-chai-matchers');
-require('solidity-coverage');
+require("hardhat-abi-exporter");
+require("@openzeppelin/hardhat-upgrades");
+require("@nomicfoundation/hardhat-chai-matchers");
+require("solidity-coverage");
 
 const {
-    OPERATOR_ID_A,
-    OPERATOR_KEY_A,
-    NETWORKS,
-    PRIVATE_KEYS,
-} = require('./utils/environment');
+  OPERATOR_ID_A,
+  OPERATOR_KEY_A,
+  NETWORKS,
+  PRIVATE_KEYS,
+} = require("./utils/environment");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-    mocha: {
-        timeout: 600000,
-        color: true,
-        failZero: Boolean(process.env.CI),
-        forbidOnly: Boolean(process.env.CI),
-        reporter: 'mocha-multi-reporters',
-        reporterOption: {
-            reporterEnabled: 'spec, mocha-junit-reporter',
-            mochaJunitReporterReporterOptions: {
-                mochaFile: 'test-results.[hash].xml',
-                includePending: true,
-                outputs: true,
-            },
-        },
+  mocha: {
+    timeout: 600000,
+    color: true,
+    failZero: Boolean(process.env.CI),
+    forbidOnly: Boolean(process.env.CI),
+    reporter: "mocha-multi-reporters",
+    reporterOption: {
+      reporterEnabled: "spec, mocha-junit-reporter",
+      mochaJunitReporterReporterOptions: {
+        mochaFile: "test-results.[hash].xml",
+        includePending: true,
+        outputs: true,
+      },
     },
-    solidity: {
-        version: '0.8.24',
-        settings: {
-            optimizer: {
-                enabled: true,
-                runs: 500,
-            },
-            evmVersion: 'cancun',
-        },
+  },
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 500,
+      },
+      evmVersion: "cancun",
     },
-    abiExporter: {
-        path: './abi',
-        runOnCompile: true
+  },
+  abiExporter: {
+    path: "./abi",
+    runOnCompile: true,
+  },
+  // defaultNetwork: 'hardhat',
+  defaultNetwork: "local",
+  // see https://v2.hardhat.org/hardhat-runner/docs/config
+  networks: {
+    hardhat: {},
+    local: {
+      url: NETWORKS.local.url,
+      chainId: NETWORKS.local.chainId,
+      gas: NETWORKS.local.gas,
+      timeout: NETWORKS.local.timeout,
+      accounts: [
+        "0xde78ff4e5e77ec2bf28ef7b446d4bec66e06d39b6e6967864b2bf3d6153f3e68",
+        // '0x748634984b480c75456a68ea88f31609cd3091e012e2834948a6da317b727c04',
+      ],
     },
-    // defaultNetwork: 'hardhat',
-    defaultNetwork: 'local',
-    // see https://v2.hardhat.org/hardhat-runner/docs/config
-    networks: {
-        hardhat: {},
-        local: {
-            url: NETWORKS.local.url,
-            chainId: NETWORKS.local.chainId,
-            gas: NETWORKS.local.gas,
-            timeout: NETWORKS.local.timeout,
-            accounts: [
-                '0xde78ff4e5e77ec2bf28ef7b446d4bec66e06d39b6e6967864b2bf3d6153f3e68',
-                // '0x748634984b480c75456a68ea88f31609cd3091e012e2834948a6da317b727c04',
-            ]
-        },
-        testnet: {
-            url: NETWORKS.testnet.url,
-            chainId: NETWORKS.testnet.chainId,
-            accounts: [
-                // add your HEX Encoded Private Key
-
-            ]
-        },
-    }
+    testnet: {
+      url: NETWORKS.testnet.url,
+      chainId: NETWORKS.testnet.chainId,
+      accounts: [
+        // add your HEX Encoded Private Key
+      ],
+    },
+  },
 };
--- system-contract-testing/test/hip-1215/hip1215-test.js
+++ system-contract-testing/test/hip-1215/hip1215-test.js
@@ -1,17 +1,21 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
-const Utils = require('../../utils/utils');
-const { GAS_LIMIT_1_000_000, GAS_LIMIT_15M, Events } = require("../../utils/constants");
-
+const {
+  GAS_LIMIT_1_000_000,
+  GAS_LIMIT_15M,
+  Events,
+} = require("../../utils/constants");
 
 describe("HIP-1215 System Contract testing", () => {
   let hip1215, mock1215;
 
   before(async () => {
     // provider configs override
-    ethers.provider.estimateGas = async() => 1_000_000;
+    ethers.provider.estimateGas = async () => 1_000_000;
     // Extract this to a fixture and run
-    const HIP1215MockFactory = await ethers.getContractFactory("HIP1215MockContract");
+    const HIP1215MockFactory = await ethers.getContractFactory(
+      "HIP1215MockContract",
+    );
     console.log("Deploy mock:");
     mock1215 = await HIP1215MockFactory.deploy();
     const HIP1215Factory = await ethers.getContractFactory("HIP1215Contract");
@@ -29,14 +33,16 @@ describe("HIP-1215 System Contract testing", () => {
         new Date().getUTCMilliseconds() + 100,
         GAS_LIMIT_1_000_000.gasLimit,
         0,
-        "0x5b8f8584"
+        "0x5b8f8584",
       );
       const transaction = await tx.wait();
       const log = transaction.logs.find(
-        e => e.fragment.name === Events.ScheduleCall
+        (e) => e.fragment.name === Events.ScheduleCall,
       );
       expect(log.args[0]).to.equal(22n);
-      expect(log.args[1]).to.equal('0x000000000000000000000000000000000000007B');
+      expect(log.args[1]).to.equal(
+        "0x000000000000000000000000000000000000007B",
+      );
       // With mocked setup we can only verify that the txn was success/revert
       expect(transaction.status).to.equal(1);
     });
@@ -88,5 +94,4 @@ describe("HIP-1215 System Contract testing", () => {
       expect(tx).to.be.false;
     });
   });
-
-}); 
+});
