e459592369d7a89c0f5fe9dfcd179827ab88b128
Glib Kozyryatskyy <glib.k@swirldslabs.com>
2025-09-29 18:54:06 +0300

    chore: timeout change
    Signed-off-by: Glib Kozyryatskyy <glib.k@swirldslabs.com>

--- system-contract-testing/hardhat.config.js
+++ system-contract-testing/hardhat.config.js
@@ -10,7 +10,7 @@ const { NETWORKS } = require("./utils/environment");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   mocha: {
-    timeout: 30000,
+    timeout: 60000,
     color: true,
     failZero: Boolean(process.env.CI),
     forbidOnly: Boolean(process.env.CI),
