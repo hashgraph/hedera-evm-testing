d5933090e35bad4ff94f714763fbe5b28d663e03
Glib Kozyryatskyy <glib.k@swirldslabs.com>
2025-12-02 16:54:46 +0200

    feature: keccak remove
    Signed-off-by: Glib Kozyryatskyy <glib.k@swirldslabs.com>

--- system-contract-testing/contracts/hip-1249/HIP1249Contract.sol
+++ system-contract-testing/contracts/hip-1249/HIP1249Contract.sol
@@ -21,23 +21,4 @@ contract HIP1249Contract {
         }
         return res;
     }
-
-    // this function should not be 'view' or 'pure' because we need transaction to be recorded on chain
-    function simulateOpsDurationThrottling1(uint iterations) public {
-        // convert uint to bytes to use as hash input
-        bytes memory value = abi.encodePacked(iterations);
-        // cycle keccak hash operation to spend some opsDuration
-        for (uint i = 0; i < iterations; i++) {
-            value = keccak(value);
-        }
-    }
-
-    function keccak(bytes memory _input) public pure returns (bytes memory) {
-        bytes32 hash;
-        assembly {
-            hash := keccak256(add(_input, 0x20), mload(_input))
-        }
-        // convert bytes32 to bytes
-        return abi.encodePacked(hash);
-    }
 }
