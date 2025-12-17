1da6eff377fe5cb650aa5724108ec6282c59d89b
Luis Mastrangelo <luis@swirldslabs.com>
2025-12-17 19:53:57 -0300

    Add HTS token create case
    Signed-off-by: Luis Mastrangelo <luis@swirldslabs.com>

--- pectra-testing/test/eip7702.test.ts
+++ pectra-testing/test/eip7702.test.ts
@@ -1,29 +1,33 @@
+import { strict as assert } from 'node:assert';
+
 import { expect } from 'chai';
 import { JsonRpcProvider, Transaction, Wallet, parseUnits, type Network } from 'ethers';
+
+import * as sdk from '@hiero-ledger/sdk';
 
 import { rpcUrl } from 'pectra-testing/config';
 import { deploy, fundEOA } from 'pectra-testing/eoa';
 import { designatorFor } from 'pectra-testing/eip7702';
 import { log } from 'pectra-testing/log';
 
-describe('eip7702', function() {
+describe('eip7702', function () {
 
     let provider: JsonRpcProvider;
     let network: Network;
 
-    before(async function() {
+    before(async function () {
         provider = new JsonRpcProvider(rpcUrl);
         network = await provider.getNetwork();
     });
 
-    describe('delegation code', function() {
+    describe('delegation code', function () {
         [
-            ...[...Array(0x11).keys()].map(i=>i+1).map(i => `0x00000000000000000000000000000000000000${i.toString(16).padStart(2, '0')}`),
+            ...[...Array(0x11).keys()].map(i => i + 1).map(i => `0x00000000000000000000000000000000000000${i.toString(16).padStart(2, '0')}`),
             '0x0000000000000000000000000000000000000167',
             '0x0000000000000000000000000000000000068cDa',
             '0xad3954AB34dE15BC33dA98170e68F0EEac294dFc',
         ].forEach(address => {
-            it(`should self-authorize address ${address}`, async function() {
+            it(`should self-authorize address ${address}`, async function () {
                 const eoa = await fundEOA();
                 const nonce = await eoa.getNonce();
 
@@ -34,11 +38,11 @@ describe('eip7702', function() {
                     gasLimit: 121_000,
                     value: 10n,
                     to: Wallet.createRandom().address,
-                    authorizationList: [ await eoa.authorize({
+                    authorizationList: [await eoa.authorize({
                         chainId: 0,
                         nonce: nonce + 1,
                         address,
-                    }) ],
+                    })],
                 }));
                 await resp.wait();
 
@@ -50,12 +54,51 @@ describe('eip7702', function() {
         });
     });
 
-    describe('e2e', function() {
-        it('should pass', async function() {
+    describe('e2e', function () {
+        it('should pass', async function () {
             const contractAddress = await deploy('Simple7702Account');
 
             const eoa1 = await fundEOA(contractAddress);
             const eoa2 = await fundEOA(contractAddress);
+
+            await (await eoa1.sendTransaction({
+                to: eoa1.address,
+            })).wait();
+
+            await (await eoa2.sendTransaction({
+                to: eoa2.address,
+            })).wait();
         });
+
+        it('should return delegation designation to `0x167` when an HTS token is created', async function () {
+            const operatorId = sdk.AccountId.fromString(process.env.OPERATOR_ID);
+            const operatorKey = sdk.PrivateKey.fromStringECDSA(process.env.OPERATOR_KEY);
+            const client = sdk.Client.forNetwork({ '127.0.0.1:50211': '0.0.3' });
+            client.setOperator(operatorId, operatorKey);
+
+            const tx = new sdk.TokenCreateTransaction({
+                tokenName: "Test",
+                tokenSymbol: "TST",
+                initialSupply: 1000n,
+                decimals: 2,
+                treasuryAccountId: operatorId,
+            });
+            const resp = await tx.execute(client);
+
+            const receipt = await resp.getReceipt(client);
+            assert(receipt.status === sdk.Status.Success, `Token creation failed with status: ${receipt.status}`);
+            log('Token Create Transaction ID:', resp.transactionId.toString(), 'Token ID', receipt.tokenId?.toString());
+
+            const tokenId = receipt.tokenId;
+            assert(tokenId !== null, 'Token ID cannot be null');
+            console.info('Transaction status:', tokenId.toString());
+            const tokenAddr = '0x' + tokenId.toEvmAddress();
+            console.info('Transaction status:', tokenAddr);
+
+            client.close();
+
+            const code = await provider.getCode(tokenAddr);
+            expect(code).to.be.equal(designatorFor('0x0000000000000000000000000000000000000167'));
+        })
     });
 });
