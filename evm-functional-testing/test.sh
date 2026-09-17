5a0f415a030622a3a44d0534fd402749d47733e9
Glib Kozyryatskyy <glib.k@swirldslabs.com>
2026-09-17 13:59:43 +0300

    test: mn missing contracts results reproducer
    Signed-off-by: Glib Kozyryatskyy <glib.k@swirldslabs.com>

--- evm-functional-testing/test.sh
+++ evm-functional-testing/test.sh
@@ -16,7 +16,7 @@ APP_PROPERTIES_PATH="local/application.properties"
 ######################### MN configs #########################
 LOCAL_MN_BUILD=false
 MIRROR_NODE_DIR="../../hiero-mirror-node"
-MIRROR_NODE_VERSION=0.163.1
+MIRROR_NODE_VERSION=0.161.3
 MIRROR_NODE_YAML_PATH="local/mn-values.yaml"
 
 ######################### Relay configs #########################
